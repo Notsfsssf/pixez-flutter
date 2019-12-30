17f8ce33c4127a12f57bed9fb27ca628a7841846
Perol_Notsf <asdplk3@live.com>
2019-12-30 15:24:22 +0800

    star icon,select image

--- lib/bloc/save_bloc.dart
+++ lib/bloc/save_bloc.dart
@@ -19,11 +19,43 @@ class SaveBloc extends Bloc<SaveEvent, SaveState> {
   @override
   SaveState get initialState => InitialSaveState();
 
+  Future<bool> saveImage(String url) async {
+    var file = await DefaultCacheManager().getFileFromCache(url);
+    Uint8List uint8list;
+    if (file == null) {
+      Directory tempDir = await getTemporaryDirectory();
+      String tempPath = tempDir.path;
+      String fullPath =
+          "$tempPath/${DateTime.now().toIso8601String()}.${url.contains("png") ? "png" : "jpg"}"; //???
+      await dio.download(url, fullPath);
+      File file = File(fullPath);
+      uint8list = await file.readAsBytes();
+    } else {
+      uint8list = await file.file.readAsBytes();
+    }
+    final _imageSaver = ImageSaver();
+    List<Uint8List> bytesList = [uint8list];
+    final res = await _imageSaver.saveImages(
+        imageBytes: bytesList, directoryName: 'pxez');
+    return res;
+  }
+
   @override
   Stream<SaveState> mapEventToState(
     SaveEvent event,
   ) async* {
-    if (event is SaveChoiceImageEvent) {}
+    if (event is SaveChoiceImageEvent) {
+      final illust = event.illusts;
+      final index = event.indexs;
+      if (illust.metaPages.isNotEmpty) {
+        for (int i = 0; i < illust.metaPages.length; i++) {
+          if (index[i]) {
+            final res = await saveImage(illust.metaPages[i].imageUrls.original);
+          }
+        }
+      } else
+        await saveImage(illust.metaSinglePage.originalImageUrl);
+    }
     if (event is SaveImageEvent) {
       try {
         final illust = event.illusts;
@@ -31,19 +63,7 @@ class SaveBloc extends Bloc<SaveEvent, SaveState> {
         final url = illust.metaPages.isNotEmpty
             ? illust.metaPages[index].imageUrls.original
             : illust.metaSinglePage.originalImageUrl;
-        var file = await DefaultCacheManager().getFileFromCache(url);
-        Uint8List uint8list;
-        if (file == null) {
-          Directory tempDir = await getTemporaryDirectory();
-          String tempPath = tempDir.path;
-          await dio.download(url, tempPath);
-        } else {
-          uint8list = await file.file.readAsBytes();
-        }
-        final _imageSaver = ImageSaver();
-        List<Uint8List> bytesList = [uint8list];
-        final res = await _imageSaver.saveImages(
-            imageBytes: bytesList, directoryName: 'pxez');
+        final res = await saveImage(url);
         yield SaveSuccesState(res);
       } catch (e) {
         print(e);
