02a38b619159b2848e90e8d6c0ed0e45d0cbbf13
perol_notsf <perol@perol-notsfdeMacBook-Pro.local>
2020-06-20 17:10:17 +0800

    34

--- lib/generated/intl/messages_zh-CN.dart
+++ lib/generated/intl/messages_zh-CN.dart
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2020. by perol_notsf, All rights reserved
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 // DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
 // This is a library that provides messages for a zh_CN locale. All the
 // messages from the main program should be duplicated here with the same
