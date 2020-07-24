36f97936d9c6f73805ae08568e726d5738b80d0c
perol <asdplk3@live.com>
2020-07-24 10:26:00 +0800

    soup

--- lib/constraint.dart
+++ lib/constraint.dart
@@ -1,4 +1,4 @@
 class Constrains {
-  static String tagName = "0.1.1";
-  static bool isGooglePlay = true;
+  static String tagName = "0.1.2";
+  static bool isGooglePlay = false;
 }
--- lib/page/soup/soup_store.dart
+++ lib/page/soup/soup_store.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:pixez/models/amwork.dart';
@@ -11,65 +12,64 @@ part 'soup_store.g.dart';
 class SoupStore = _SoupStoreBase with _$SoupStore;
 
 abstract class _SoupStoreBase with Store {
- final dio = Dio(BaseOptions(headers: {
-   HttpHeaders.acceptLanguageHeader:ApiClient.Accept_Language,
+  final dio = Dio(BaseOptions(headers: {
+    HttpHeaders.acceptLanguageHeader: ApiClient.Accept_Language,
     'user-agent':
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.26 Safari/537.36 Edg/85.0.564.13',
     HttpHeaders.refererHeader: 'https://www.pixivision.net/zh/',
   }));
   ObservableList<AmWork> amWorks = ObservableList();
   @observable
   String description;
+
   @action
   fetch(String url) async {
     Response response = await dio.request(url);
     print(response.data);
     var document = parse(response.data);
-    var workInfo = document.getElementsByClassName("am__body");
-    var nodes = workInfo.first.nodes;
-    var amWorkGtmDocument = parse(workInfo.first.innerHtml);
- description = document
-        .getElementsByClassName('am__description _medium-editor-text')
+    var workInfo = document
+        .getElementsByTagName("article")
         .first
-        .innerHtml
-        .replaceAll('<p>', '')
-        .replaceAll('</p>', '')
-        .replaceAll('</br>', '')
-        .replaceAll('<br>', '');
-    for (int i = 1; i <= nodes.length; i++) {
+        .getElementsByTagName('header');
+    var nodes = document
+        .getElementsByTagName("article")
+        .first
+        .getElementsByClassName('am__body')
+        .first
+        .children;
+    description = workInfo.first
+        .getElementsByTagName('p')
+        .map((e) => e.text)
+        .toList()
+        .toString().replaceAll('[', '').replaceAll(']', '').replaceAll(',', '');
+    amWorks.clear();
+    for (var value in nodes) {
       try {
+        if(!value.attributes['class'].contains('illust')){
+          continue;
+        }
         AmWork amWork = AmWork();
-        var amWorkGtm = amWorkGtmDocument.getElementsByClassName(
-            "am__work gtm__illust-collection-illusts-${i}");
-        var infoDoc = parse(amWorkGtm.first.innerHtml)
-            .getElementsByClassName("am__work__info");
-        var link = infoDoc.first.getElementsByTagName("a");
-        amWork.title = infoDoc.first
-            .getElementsByTagName('h3')
-            .first
-            .getElementsByTagName('a')
-            .first
-            .text;
-        amWork.user = infoDoc.first
-            .getElementsByTagName('p')
-            .first
-            .getElementsByTagName('a')
-            .first
-            .text;
-        amWork.userImage =
-            infoDoc.first.getElementsByTagName('img').first.attributes['src'];
-        amWork.userLink = link.first.attributes['href'];
-        var mainDoc = parse(amWorkGtm.first.innerHtml)
-            .getElementsByClassName("am__work__main");
-        amWork.arworkLink =
-            mainDoc.first.getElementsByTagName('a').first.attributes['href'];
-        amWork.showImage =
-            mainDoc.first.getElementsByTagName('img').first.attributes['src'];
-        amWorks.clear();
+        for (var aa in value.getElementsByTagName('a')) {
+          var a = aa.attributes['href'];
+          if (a.contains('https://www.pixiv.net/artworks')) {
+            amWork.arworkLink = a;
+            amWork.showImage =
+                value.getElementsByTagName('img')[1].attributes['src'];
+            amWork.title = value.getElementsByTagName('h3').first.text;
+          }
+          if (a.contains('https://www.pixiv.net/users')) {
+            amWork.userLink = a;
+            amWork.user = value.getElementsByTagName('p').first.text;
+            amWork.userImage =
+                value.getElementsByTagName('img').first.attributes['src'];
+          }
+        }
+        if (amWork.userLink == null || amWork.arworkLink == null) {
+          continue;
+        }
         amWorks.add(amWork);
       } catch (e) {
         print(e);
-        continue;
       }
     }
   }
