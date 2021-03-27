b197c65eb90e84e1158330d9a92bc765a9357976
perol <asdplk3@live.com>
2021-03-27 21:41:35 +0800

    sssgridman timeline

--- ios/SSSGridman/SSSGridman.swift
+++ ios/SSSGridman/SSSGridman.swift
@@ -11,47 +11,55 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent(), i: UIImage(named: "ic_launcher-playstore"))
     }
-
+    
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry = SimpleEntry(date: Date(), configuration: configuration, i: UIImage(named: "ic_launcher-playstore"))
         completion(entry)
     }
-
+    
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+        guard let url = URL(string: "https://i.pximg.net/img-master/img/2018/12/18/00/08/54/72162700_p0_master1200.jpg") else { return }
+        var request = URLRequest(url: url)
+        request.setValue("https://app-api.pixiv.net/", forHTTPHeaderField: "referer")
+        request.setValue("PixivIOSApp/5.8.0", forHTTPHeaderField: "User-Agent")
+        URLSession.shared.dataTask(with: request){ (data, response, error) in
+            if let image = UIImage(data: data!){
+                let currentDate = Date()
+                let entry = SimpleEntry(date: currentDate, configuration: configuration, i: image)
+                entries.append(entry)
+                let timeline = Timeline(entries: entries, policy: .atEnd)
+                completion(timeline)
+            }
+            else{
+                print(error ?? "")
+            }
+        }.resume()
+        
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let configuration: ConfigurationIntent
+    let i:UIImage?
 }
 
 struct SSSGridmanEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        Image(uiImage: entry.i!).resizable().scaledToFill()
     }
+    
 }
 
 @main
 struct SSSGridman: Widget {
     let kind: String = "SSSGridman"
-
+    
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             SSSGridmanEntryView(entry: entry)
@@ -63,7 +71,7 @@ struct SSSGridman: Widget {
 
 struct SSSGridman_Previews: PreviewProvider {
     static var previews: some View {
-        SSSGridmanEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        SSSGridmanEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), i: UIImage(named: "ic_launcher-playstore")))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
