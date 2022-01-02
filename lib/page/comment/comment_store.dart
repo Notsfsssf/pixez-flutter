6d1eb602c320996d61ffca051168754ef799b8d4
perol <asdplk3@live.com>
2022-01-02 15:00:42 +0800

    pixiv emojis fetcher and assets

--- lib/page/comment/comment_store.dart
+++ lib/page/comment/comment_store.dart
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020. by perol_notsf, All rights reserved
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import 'dart:io';
 import 'dart:math';
 
@@ -12,6 +28,47 @@ part 'comment_store.g.dart';
 class CommentStore = _CommentStoreBase with _$CommentStore;
 
 abstract class _CommentStoreBase with Store {
+  final emojisMap = {
+    '(normal)': '101.png',
+    '(surprise)': '102.png',
+    '(serious)': '103.png',
+    '(heaven)': '104.png',
+    '(happy)': '105.png',
+    '(excited)': '106.png',
+    '(sing)': '107.png',
+    '(cry)': '108.png',
+    '(normal2)': '201.png',
+    '(shame2)': '202.png',
+    '(love2)': '203.png',
+    '(interesting2)': '204.png',
+    '(blush2)': '205.png',
+    '(fire2)': '206.png',
+    '(angry2)': '207.png',
+    '(shine2)': '208.png',
+    '(panic2)': '209.png',
+    '(normal3)': '301.png',
+    '(satisfaction3)': '302.png',
+    '(surprise3)': '303.png',
+    '(smile3)': '304.png',
+    '(shock3)': '305.png',
+    '(gaze3)': '306.png',
+    '(wink3)': '307.png',
+    '(happy3)': '308.png',
+    '(excited3)': '309.png',
+    '(love3)': '310.png',
+    '(normal4)': '401.png',
+    '(surprise4)': '402.png',
+    '(serious4)': '403.png',
+    '(love4)': '404.png',
+    '(shine4)': '405.png',
+    '(sweat4)': '406.png',
+    '(shame4)': '407.png',
+    '(sleep4)': '408.png',
+    '(heart)': '501.png',
+    '(teardrop)': '502.png',
+    '(star)': '503.png'
+  };
+
   String? nextUrl;
   @observable
   ObservableList<Comment> comments = ObservableList();
@@ -24,7 +81,7 @@ abstract class _CommentStoreBase with Store {
   int? pId;
   final bool isReplay;
 
-  _CommentStoreBase(this._controller, this.id,this.pId, this.isReplay);
+  _CommentStoreBase(this._controller, this.id, this.pId, this.isReplay);
 
   @action
   fetch() async {
