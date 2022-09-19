1c63ac68cdfea0383758785af656f9c39c481a45
perol <asdplk3@live.com>
2022-09-19 23:10:14 +0800

    target 33 notification

--- lib/constants.dart
+++ lib/constants.dart
@@ -15,7 +15,7 @@
  */
 
 class Constants {
-  static String tagName = "0.9.5";
+  static String tagName = "0.9.6";
   static const isGooglePlay =
       bool.fromEnvironment("IS_GOOGLEPLAY", defaultValue: false);
   static int type = 0;
