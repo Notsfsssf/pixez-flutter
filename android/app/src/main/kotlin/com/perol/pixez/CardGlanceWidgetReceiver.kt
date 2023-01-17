4573b9000d80860edc5e0331661588c280f94771
perol <asdplk3@live.com>
2023-01-17 21:50:05 +0800

    remove glance dep

--- android/app/src/main/kotlin/com/perol/pixez/CardGlanceWidgetReceiver.kt
+++ android/app/src/main/kotlin/com/perol/pixez/CardGlanceWidgetReceiver.kt
@@ -1,10 +1,10 @@
-package com.perol.pixez
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import com.perol.pixez.glance.ImageGlanceWidget
-
-class CardGlanceWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget
-        get() = ImageGlanceWidget()
-}
+//package com.perol.pixez
+//
+//import androidx.glance.appwidget.GlanceAppWidget
+//import androidx.glance.appwidget.GlanceAppWidgetReceiver
+//import com.perol.pixez.glance.ImageGlanceWidget
+//
+//class CardGlanceWidgetReceiver : GlanceAppWidgetReceiver() {
+//    override val glanceAppWidget: GlanceAppWidget
+//        get() = ImageGlanceWidget()
+//}
