7e4d0005f119d301eae19bba56b8cc323fa85c45
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-05-10 17:32:40 +0800

    :technologist: 改善调试时体验
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- lib/windows.dart
+++ lib/windows.dart
@@ -1,7 +1,28 @@
 import 'dart:ffi';
+import 'dart:io';
 
 import 'package:ffi/ffi.dart';
+import 'package:flutter/foundation.dart';
 import 'package:win32/winrt.dart';
+import 'package:win32_registry/win32_registry.dart';
+
+registerProtocol(String scheme, String desc, String template) {
+  // 向操作系统注册协议
+  // 仅在调试时使用这个, 发布时使用msix 已经自动注册了所以不需要手动修改了
+  if (!kDebugMode) return;
+
+  String appPath = Platform.resolvedExecutable;
+
+  String protocolRegKey = 'Software\\Classes\\$scheme';
+  const protocolCmdRegKey = 'shell\\open\\command';
+
+  final regKey = Registry.currentUser.createKey(protocolRegKey);
+  regKey.createValue(RegistryValue('', RegistryValueType.string, desc));
+  regKey.createValue(
+      const RegistryValue('URL Protocol', RegistryValueType.string, ''));
+  regKey.createKey(protocolCmdRegKey).createValue(
+      RegistryValue('', RegistryValueType.string, '$appPath $template'));
+}
 
 int getAccentColor() {
   final settings = UISettings();
