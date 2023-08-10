8826376633d6fed34be46b7be40992484020638c
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-08-10 09:15:00 +0800

    :construction: 添加几个状态
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- lib/constants.dart
+++ lib/constants.dart
@@ -24,5 +24,12 @@ class Constants {
   static String? code_verifier = null;
 
   /// 为true表示使用FluentUI 否则为false,不应作为Desktop的判断
-  static bool isFluent = Platform.isWindows;
+  static final bool isFluent = Platform.isWindows;
+
+  /// 是否是桌面平台 这个字段会影响列表加载更多的方式
+  static final bool isDesktop =
+      Platform.isWindows || Platform.isMacOS || Platform.isLinux;
+
+  /// 当使用鼠标滚轮滚动时 距离底边还有多少距离时开始加载下一页
+  static const double lazyLoadSize = 300;
 }
