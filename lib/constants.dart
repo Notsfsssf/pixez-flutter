a159c8521cf2f1d8b3c677f703337918f9244bbc
perol <asdplk3@live.com>
2023-11-19 23:53:43 +0800

    android 0.9.33

--- lib/constants.dart
+++ lib/constants.dart
@@ -17,7 +17,7 @@
 import 'dart:io';
 
 class Constants {
-  static String tagName = "0.9.32";
+  static String tagName = "0.9.33";
   static const isGooglePlay =
       bool.fromEnvironment("IS_GOOGLEPLAY", defaultValue: false);
   static int type = 0;
