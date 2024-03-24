42504f0d8c688bf1e8e79253b85fb876dff5aeb3
NightFeather <leolee50910@gmail.com>
2024-03-25 07:26:43 +0800

    fix: remove unused code

--- lib/page/hello/hello_page.dart
+++ lib/page/hello/hello_page.dart
@@ -15,7 +15,6 @@
  */
 
 import 'dart:async';
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pixez/component/painter_avatar.dart';
@@ -31,7 +30,6 @@ import 'package:pixez/page/hello/recom/recom_spotlight_page.dart';
 import 'package:pixez/page/hello/setting/setting_page.dart';
 import 'package:pixez/page/preview/preview_page.dart';
 import 'package:pixez/page/search/search_page.dart';
-import 'package:pixez/utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HelloPage extends StatefulWidget {
@@ -64,9 +62,6 @@ class _HelloPageState extends State<HelloPage> {
     });
     initLinksStream();
     initPlatformState();
-    if (Platform.isAndroid || Platform.isIOS) {
-      configSecureWindow(userSetting.secureWindow);
-    }
   }
 
   Future<void> initPlatformState() async {
