9fbe53a9620bafa2b4de2be846ce930d11bff673
perol <asdplk3@live.com>
2025-03-03 00:01:19 +0800

    export novel init

--- lib/page/novel/history/novel_history_store.dart
+++ lib/page/novel/history/novel_history_store.dart
@@ -12,9 +12,15 @@
  *  You should have received a copy of the GNU General Public License along with
  *  this program. If not, see <http://www.gnu.org/licenses/>.
  */
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
+import 'package:pixez/er/sharer.dart';
 import 'package:pixez/models/novel_persist.dart';
 import 'package:pixez/models/novel_recom_response.dart';
+import 'package:pixez/saf_plugin.dart';
 
 part 'novel_history_store.g.dart';
 
@@ -56,5 +62,38 @@ abstract class _NovelHistoryStoreBase with Store {
     await novelPersistProvider.open();
     await novelPersistProvider.deleteAll();
     await fetch();
+  }
+
+  Future<void> importData() async {
+    final result = await SAFPlugin.openFile();
+    if (result == null) return;
+    final json = utf8.decode(result);
+    final decoder = JsonDecoder();
+    List<dynamic> maps = decoder.convert(json);
+    maps.forEach((illust) {
+      var novelMap = Map.from(illust);
+      var novelPersist = NovelPersist(
+          novelId: novelMap['novel_id'],
+          userId: novelMap['user_id'],
+          pictureUrl: novelMap['picture_url'],
+          time: novelMap['time'],
+          title: novelMap['title'],
+          userName: novelMap['user_name']);
+      novelPersistProvider.insert(novelPersist);
+    });
+  }
+
+  Future<void> exportData(BuildContext context) async {
+    await novelPersistProvider.open();
+    final exportData = await novelPersistProvider.getAllAccount();
+    final uint8List = utf8.encode(jsonEncode(exportData));
+    if (Platform.isIOS) {
+      await Sharer.exportUint8List(context, uint8List, "novelpersist.json");
+    } else {
+      final uriStr =
+          await SAFPlugin.createFile("novelpersist.json", "application/json");
+      if (uriStr == null) return;
+      await SAFPlugin.writeUri(uriStr, uint8List);
+    }
   }
 }
