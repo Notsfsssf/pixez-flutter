bf8d190deb8601c1b074e2414dfe83381143f8e9
perol <asdplk3@live.com>
2025-07-13 23:29:55 +0800

    0.9.74

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -80,8 +80,8 @@ android {
         applicationId = packageName
         minSdk = 21
         targetSdk = 35
-        versionCode = 10009730
-        versionName = "0.9.73 L"
+        versionCode = 10009740
+        versionName = "0.9.74 ra"
         ndk {
             abiFilters.addAll(arrayOf("armeabi-v7a", "arm64-v8a", "x86_64"))
         }
