1f4c68f118a29b62ab40ad91272e3739b48a2e16
F16TH <1423147015@qq.com>
2025-10-06 20:05:39 +0800

    在图片左上角加入了R-18和R-18G标签

--- lib/component/illust_card.dart
+++ lib/component/illust_card.dart
@@ -195,6 +195,11 @@ class _IllustCardState extends State<IllustCard> {
                       Positioned.fill(child: _buildPic(tag, tooLong)),
                       Positioned(
                           top: 5.0,
+                          left: 5.0,
+                          child: _buildR18Badge(),
+                      ),
+                      Positioned(
+                          top: 5.0,
                           right: 5.0,
                           child: Row(
                             children: [
@@ -299,6 +304,31 @@ class _IllustCardState extends State<IllustCard> {
         child: Text(
           "AI",
           style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildR18Badge() {
+    String text = "";
+    if (store.illusts!.xRestrict == 2) {
+      text = "R-18G";
+    } else if (store.illusts!.xRestrict == 1) {
+      text = "R-18";
+    }
+
+    if (text.isEmpty) return Container();
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 4.0),
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 12),
         ),
       ),
     );
