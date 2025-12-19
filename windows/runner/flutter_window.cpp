3856a1e602ef4840b1a120367bacd74fed8b6be1
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-12-19 10:19:24 +0800

    feat(windows): 引入 sqlite3 依赖并优化插件注册管理
    将 sqlite3 添加为直接依赖，并更新 CMakeLists.txt 和 pubspec.yaml 配置，
    以支持新的数据库集成。同时重构 Windows 平台的插件注册逻辑，
    通过引入 plugins_manager 来统一管理原生插件初始化流程，
    提升代码结构清晰度与可维护性。
    
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -1,28 +1,9 @@
 #include "flutter_window.h"
+#include "plugins_manager.h"
 
 #include <optional>
 
 #include "flutter/generated_plugin_registrant.h"
-
-#include <windows.h>
-#include <ShlObj.h>
-#include <iostream>
-#include <flutter/binary_messenger.h>
-#include <flutter/standard_method_codec.h>
-#include <flutter/method_channel.h>
-#include <flutter/method_result_functions.h>
-#include <flutter/encodable_value.h>
-#include <fstream>
-#include <stdexcept>
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "plugins/clipboard_plugin.h"
-#include "plugins/document_plugin.h"
-#include "plugins/paths_plugin.h"
-#include "plugins/single_instance_plugin.h"
-#include "plugins/weiss_plugin.h"
-#include "plugins/win32_plugin.h"
 
 FlutterWindow::FlutterWindow(const flutter::DartProject &project)
     : project_(project) {}
@@ -48,16 +29,8 @@ bool FlutterWindow::OnCreate()
     return false;
   }
   RegisterPlugins(flutter_controller_->engine());
+  RegisterPixEzPlugins(flutter_controller_->engine(), GetHandle());
   SetChildContent(flutter_controller_->view()->GetNativeWindow());
-
-  const auto &codec = flutter::StandardMethodCodec::GetInstance();
-  auto messenger = flutter_controller_->engine()->messenger();
-  Clipboard::Initialize(messenger, &codec);
-  Document::Initialize(messenger, &codec, GetHandle());
-  Paths::Initialize(messenger, &codec);
-  SingleInstance::Initialize(messenger, &codec);
-  Weiss::Initialize(messenger, &codec);
-  Win32::Initialize(messenger, &codec);
 
   flutter_controller_->engine()->SetNextFrameCallback([&]()
                                                       { this->Show(); });
