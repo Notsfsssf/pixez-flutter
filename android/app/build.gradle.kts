95a988e06cc9818b9ed772cfe50d17a11ccdeab2
perol <asdplk3@live.com>
2025-12-21 22:54:24 +0800

    0.9.83 sort

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -80,8 +80,8 @@ android {
         applicationId = packageName
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 10009821
-        versionName = "0.9.83 X"
+        versionCode = 10009830
+        versionName = "0.9.83 sort"
         ndk {
             abiFilters.addAll(arrayOf("armeabi-v7a", "arm64-v8a", "x86_64"))
         }
