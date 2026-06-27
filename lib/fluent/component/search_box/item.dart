dd4fc57fde7623ccbe8041632bdf7113f31ad25a
舰队的偶像-岛风酱! <frg2089@outlook.com>
2026-06-28 00:10:50 +0800

    feat(search): 重构搜索框组件
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- lib/fluent/component/search_box/item.dart
+++ lib/fluent/component/search_box/item.dart
@@ -1,219 +1,86 @@
 part of 'pixez_search_box.dart';
 
-enum _PixEzSearchBoxItemType {
-  normal,
-  history,
-  tag,
-  illustId,
-  painterId,
-  pixivisionId,
-  cleanHistory,
+class _NextPixEzSearchBoxItem
+    extends AutoSuggestBoxItem<_NextPixEzSearchBoxItemValue> {
+  _NextPixEzSearchBoxItem({required super.value, required super.label});
+
+  factory _NextPixEzSearchBoxItem.cleanHistory(BuildContext context) =>
+      _NextPixEzSearchBoxItem(
+        value: _ClearTagsPersistsItemValue(),
+        label: I18n.of(context).clear_search_tag_history,
+      );
+  factory _NextPixEzSearchBoxItem.tagsPersist(
+    BuildContext context,
+    TagsPersist tags,
+  ) => _NextPixEzSearchBoxItem(
+    value: _TagsPersistItemValue(tags),
+    label: '${tags.name} ${tags.translatedName}',
+  );
+  factory _NextPixEzSearchBoxItem.trendTags(
+    BuildContext context,
+    TrendTags tags,
+  ) => _NextPixEzSearchBoxItem(
+    value: _TrendTagsItemValue(tags),
+    label: '#${tags.tag} ${tags.translatedName}',
+  );
+  factory _NextPixEzSearchBoxItem.tags(BuildContext context, Tags tags) =>
+      _NextPixEzSearchBoxItem(
+        value: _TagsItemValue(tags),
+        label: '${tags.name} ${tags.translated_name}',
+      );
+  factory _NextPixEzSearchBoxItem.illustId(BuildContext context, int id) =>
+      _NextPixEzSearchBoxItem(
+        value: _IllustIdItemValue(id),
+        label: id.toString(),
+      );
+  factory _NextPixEzSearchBoxItem.painterId(BuildContext context, int id) =>
+      _NextPixEzSearchBoxItem(
+        value: _PainterIdItemValue(id),
+        label: id.toString(),
+      );
+  factory _NextPixEzSearchBoxItem.pixivisionId(BuildContext context, int id) =>
+      _NextPixEzSearchBoxItem(
+        value: _PixivisionIdItemValue(id),
+        label: id.toString(),
+      );
 }
 
-class _PixEzSearchBoxItem {
-  final _PixEzSearchBoxItemType type;
-  final String? word;
-  final String? translated;
-  final int? id;
+abstract class _NextPixEzSearchBoxItemValue {}
 
-  const _PixEzSearchBoxItem({
-    this.type = _PixEzSearchBoxItemType.normal,
-    this.word = null,
-    this.translated = null,
-    this.id = null,
-  });
+class _ClearTagsPersistsItemValue extends _NextPixEzSearchBoxItemValue {}
+
+class _TagsPersistItemValue extends _NextPixEzSearchBoxItemValue {
+  final TagsPersist data;
+
+  _TagsPersistItemValue(this.data);
 }
 
-class _PixEzSearchItem extends AutoSuggestBoxItem<_PixEzSearchBoxItem> {
-  _PixEzSearchItem({
-    required super.value,
-    required super.label,
-    super.child,
-    super.onSelected,
-  });
+class _TrendTagsItemValue extends _NextPixEzSearchBoxItemValue {
+  final TrendTags data;
 
-  static Widget _buildTagImage(BuildContext context, TrendTags tags) {
-    return PixEzButton(
-      noPadding: true,
-      child: PixivImage(
-        tags.illust.imageUrls.squareMedium,
-        fit: BoxFit.cover,
-        height: 26,
-      ),
-      onPressed: () {
-        Leader.push(
-          context,
-          IllustLightingPage(id: tags.illust.id),
-          icon: Icon(FluentIcons.picture),
-          title: Text(
-            I18n.of(context).illust_id + ': ${tags.illust.id}',
-          ),
-        );
-      },
-    );
-  }
+  _TrendTagsItemValue(this.data);
+}
 
-  factory _PixEzSearchItem.build({
-    required BuildContext context,
-    required String title,
-    required _PixEzSearchBoxItem value,
-    String? label = null,
-    String? subtitle = null,
-    Widget? leading = null,
-    Widget? trailing = null,
-    void Function()? onSelected,
-    bool reverse = false,
-  }) {
-    label ??= '$title $subtitle';
+class _TagsItemValue extends _NextPixEzSearchBoxItemValue {
+  final Tags data;
 
-    var text = [
-      TextSpan(
-        text: title,
-        style: FluentTheme.of(context).typography.body?.copyWith(
-              color: FluentTheme.of(context).accentColor,
-            ),
-      ),
-      TextSpan(
-        text: ' ',
-        style: null,
-      ),
-      TextSpan(
-        text: subtitle,
-        style: null,
-      )
-    ];
+  _TagsItemValue(this.data);
+}
 
-    if (reverse && subtitle != null) text = text.reversed.toList();
+class _IllustIdItemValue extends _NextPixEzSearchBoxItemValue {
+  final int id;
 
-    return _PixEzSearchItem(
-      child: Row(
-        children: [
-          if (leading != null)
-            Padding(
-              padding: EdgeInsets.only(right: 4.0),
-              child: leading,
-            ),
-          Expanded(
-            child: Tooltip(
-              richMessage: TextSpan(
-                children: text,
-              ),
-              child: Text.rich(
-                TextSpan(
-                  children: text,
-                ),
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-          ),
-          if (trailing != null)
-            Padding(
-              padding: EdgeInsets.only(left: 4.0),
-              child: trailing,
-            ),
-        ],
-      ),
-      label: label,
-      value: value,
-      onSelected: onSelected,
-    );
-  }
+  _IllustIdItemValue(this.id);
+}
 
-  /// 清空历史记录
-  factory _PixEzSearchItem.cleanHistory(BuildContext context) {
-    return _PixEzSearchItem.build(
-      context: context,
-      leading: Icon(FluentIcons.delete),
-      title: I18n.of(context).clear_search_tag_history,
-      value: _PixEzSearchBoxItem(
-        type: _PixEzSearchBoxItemType.cleanHistory,
-      ),
-    );
-  }
-  factory _PixEzSearchItem.tagsPersist(
-      BuildContext context, TagsPersist tags, void Function() refresh) {
-    return _PixEzSearchItem.build(
-      context: context,
-      title: tags.name,
-      subtitle: tags.translatedName,
-      value: _PixEzSearchBoxItem(
-        type: _PixEzSearchBoxItemType.history,
-        word: tags.name,
-        translated: tags.translatedName,
-      ),
-      trailing: PixEzButton(
-        child: Icon(FluentIcons.chrome_close),
-        onPressed: () {
-          assert(tags.id != null);
-          tagHistoryStore.delete(tags.id!);
-          refresh();
-        },
-      ),
-    );
-  }
+class _PainterIdItemValue extends _NextPixEzSearchBoxItemValue {
+  final int id;
 
-  factory _PixEzSearchItem.trendTags(BuildContext context, TrendTags tags) {
-    return _PixEzSearchItem.build(
-      context: context,
-      title: '#${tags.tag}',
-      subtitle: tags.translatedName != null ? '#${tags.translatedName}' : null,
-      leading: _buildTagImage(context, tags),
-      value: _PixEzSearchBoxItem(
-        word: tags.tag,
-        translated: tags.translatedName,
-      ),
-    );
-  }
+  _PainterIdItemValue(this.id);
+}
 
-  factory _PixEzSearchItem.tags(BuildContext context, Tags tags, String text) {
-    return _PixEzSearchItem.build(
-      context: context,
-      title: tags.name,
-      subtitle: tags.translated_name,
-      label: text,
-      value: _PixEzSearchBoxItem(
-        word: tags.name,
-        translated: tags.translated_name,
-        type: _PixEzSearchBoxItemType.tag,
-      ),
-    );
-  }
-  factory _PixEzSearchItem.illustId(BuildContext context, int id) {
-    return _PixEzSearchItem.build(
-      context: context,
-      reverse: true,
-      title: id.toString(),
-      subtitle: I18n.of(context).illust_id,
-      value: _PixEzSearchBoxItem(
-        type: _PixEzSearchBoxItemType.illustId,
-        id: id,
-      ),
-    );
-  }
+class _PixivisionIdItemValue extends _NextPixEzSearchBoxItemValue {
+  final int id;
 
-  factory _PixEzSearchItem.painterId(BuildContext context, int id) {
-    return _PixEzSearchItem.build(
-      context: context,
-      reverse: true,
-      title: id.toString(),
-      subtitle: I18n.of(context).painter_id,
-      value: _PixEzSearchBoxItem(
-        type: _PixEzSearchBoxItemType.painterId,
-        id: id,
-      ),
-    );
-  }
-
-  factory _PixEzSearchItem.pixivisionId(BuildContext context, int id) {
-    return _PixEzSearchItem.build(
-      context: context,
-      reverse: true,
-      title: id.toString(),
-      subtitle: 'Pixivision Id',
-      value: _PixEzSearchBoxItem(
-        type: _PixEzSearchBoxItemType.pixivisionId,
-        id: id,
-      ),
-    );
-  }
+  _PixivisionIdItemValue(this.id);
 }
